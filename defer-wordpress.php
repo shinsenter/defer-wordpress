1150b2766d31c5206f18e2529ab28dfc268f3e67
Shin <2082119+shinsenter@users.noreply.github.com>
2021-03-09 14:20:07 +0900

    2.0.0-beta.21

--- defer-wordpress.php
+++ defer-wordpress.php
@@ -28,7 +28,7 @@ if (!defined('WPINC')) {
  * Plugin Name:       An efficient lazy loader (defer.js)
  * Plugin URI:        https://wordpress.org/plugins/shins-pageload-magic/
  * Description:       🚀 A native, blazing fast lazy loader. ✅ Legacy browsers support (IE9+). 💯 SEO friendly. 🧩 Lazy load almost anything.
- * Version:           2.0.0-beta.20
+ * Version:           2.0.0-beta.21
  * Author:            Mai Nhut Tan
  * Author URI:        https://code.shin.company/
  * License:           GPL-2.0+
@@ -44,7 +44,7 @@ if (!defined('WPINC')) {
 if (!defined('DEFER_WP_PLUGIN_VERSION')) {
     define('DEFER_WP_PLUGIN_BASE', plugin_basename(__FILE__));
     define('DEFER_WP_PLUGIN_NAME', 'defer-wordpress');
-    define('DEFER_WP_PLUGIN_VERSION', '2.0.0-beta.20');
+    define('DEFER_WP_PLUGIN_VERSION', '2.0.0-beta.21');
     define('DEFER_WP_PLUGIN_PREFIX', DEFER_WP_PLUGIN_NAME . '_');
 
     define('DEFER_WP_PLUGIN_HOOK', 'plugin_action_links_' . DEFER_WP_PLUGIN_BASE);
@@ -58,7 +58,7 @@ if (!defined('DEFER_WP_PLUGIN_VERSION')) {
     $has_deferjs = file_exists(__DIR__ . '/public/lib/defer_plus.min.js');
     define('DEFER_WP_SRC_DEFERJS_CDN', $has_deferjs
         ? __DIR__ . '/public/lib/defer_plus.min.js'
-        : 'https://cdn.jsdelivr.net/npm/@shinsenter/defer.js@2.0.0/dist/defer_plus.min.js');
+        : 'https://cdn.jsdelivr.net/npm/@shinsenter/defer.js@2.1.0/dist/defer_plus.min.js');
 
     $has_polyfill = file_exists(__DIR__ . '/public/lib/polyfill.min.js');
     define('DEFER_WP_SRC_POLYFILL_CDN', $has_polyfill
